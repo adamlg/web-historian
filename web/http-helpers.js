88eab8d425dc51c8d763661e6348603e2a6584bf
MKS-Student <mks.student@hackreactor.com>
2015-03-27 17:35:38 -0500

    2 tests left.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,7 +14,20 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  asset = asset.substr(1, asset.length)
+  fs.readFile('./archives/sites/'+asset, {encoding: 'utf-8'}, function(err, data) {
+    if(err) {
+      res.writeHead(404, headers)
+      res.end()
+    }
+    res.writeHead(200, headers)
+    res.end(data)
+  })
 };
+
+
+
+
 
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,59 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var stat = require('./http-helpers.js')
 // require more modules/folders here!
 
+var routes = {
+  'GET': {},
+  'POST': {},
+  'OPTIONS': {}
+}
+
+var Router = {
+  'get': function(path, cb) {
+    routes['GET'][path] = cb
+  },
+  'post': function(path, cb) {
+    routes['POST'][path] = cb
+  },
+  'options': function(path, cb) {
+    routes['OPTIONS'][path] = cb
+  }
+}
+
+Router.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  var file = fs.createReadStream('web/public/index.html')
+  file.pipe(res)
+})
+
+Router.post('/', function(req, res) {
+  var file = fs.createWriteStream('archives/sites/sites.txt', {'flags': 'a'})
+  var postedURLString = req.url + '\n'
+  req.on('data', function(chunk) {
+    file.write(chunk.toString())
+  })
+  req.on('end', function() {
+    file.write('\n')
+    file.end()
+    res.writeHead(302, stat.headers)
+    res.end()
+    
+  })
+
+})
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(routes[req.method]){
+    if(routes[req.method][req.url]){
+      routes[req.method][req.url](req, res)
+    } else {
+      stat.serveAssets(res, req.url)
+    }
+  } else {
+    res.writeHead(400)
+    res.end()
+  }
+  //res.end(archive.paths.list);
 };
